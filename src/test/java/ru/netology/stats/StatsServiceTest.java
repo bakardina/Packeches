1a414789bf9be0b4f88ee0a6adcc0647285be6aa
bakardina <suntiflis@gmail.com>
2022-10-19 16:50:46 +0300

    chor: initial commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,28 @@
+package ru.netology.stats;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatsServiceTest {
+
+    @Test
+    public void test() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.countBelowAvg(sales);
+        long expected = 5;
+
+        assertEquals(expected, actual);
+    }
+}
+
